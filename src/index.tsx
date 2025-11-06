f1785853827062364bace821cae60339ee349d18
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-07 02:03:15 +0900

    feat: MUI standard integration

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
+import CssBaseLine from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 import "./css/index.css";
 
 const container = document.getElementById("root")!;
@@ -12,12 +15,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
