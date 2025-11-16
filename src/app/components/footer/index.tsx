a3cab3020159b821906f521ca49c8f165a601059
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-11-17 00:46:54 +0900

    fix: organize homepage related sectional components

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
         <Stack flexDirection={"row"} sx={{ mt: "94px" }}>
           <Stack flexDirection={"column"} style={{ width: "340px" }}>
             <Box>
-              <img width={"100px"} src={"/icons/burak.svg"} />
+              <img width={"100px"} src={"/icons/burak.svg"} alt="brand-logo" />
             </Box>
             <Box className={"foot-desc-txt"}>
               Focusing on the gourmet Turkish breakfast as well as the youth
@@ -28,10 +28,10 @@ export default function Footer() {
               Burak Gurme creates an illusion with its cuisine.
             </Box>
             <Box className="sns-context">
-              <img src={"/icons/facebook.svg"} />
-              <img src={"/icons/twitter.svg"} />
-              <img src={"/icons/instagram.svg"} />
-              <img src={"/icons/youtube.svg"} />
+              <img src={"/icons/facebook.svg"} alt="facebook" />
+              <img src={"/icons/twitter.svg"} alt="twitter" />
+              <img src={"/icons/instagram.svg"} alt="instagram" />
+              <img src={"/icons/youtube.svg"} alt="youtube" />
             </Box>
           </Stack>
           <Stack sx={{ ml: "288px" }} flexDirection={"row"}>
