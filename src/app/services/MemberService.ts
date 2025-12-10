551daa05b15e65a41219730748d6da3fb82770fb
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-12-10 23:39:04 +0900

    fix: modify ChosenProduct component logic

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -20,6 +20,17 @@ class MemberService {
       throw err;
     }
   }
+  public async getRestaurant(): Promise<Member> {
+    try {
+      const url = this.path + "/member/restaurant";
+      const result = await axios.get(url);
+      const restaurant: Member = result.data;
+      return restaurant;
+    } catch (err) {
+      console.log("Error, getRestaurant:", err);
+      throw err;
+    }
+  }
 }
 
 export default MemberService;
