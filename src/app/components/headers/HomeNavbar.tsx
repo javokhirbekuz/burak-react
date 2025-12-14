642b50b80b60df73704b1771c536f85e065699e4
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-12-14 21:40:24 +0900

    feat: create signup process

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,7 +1,6 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
-import { useEffect, useState } from "react";
 import { CartItem } from "../../lib/types/search";
 
 interface HomeNavbarProps {
@@ -10,24 +9,24 @@ interface HomeNavbarProps {
   onRemove: (item: CartItem) => void;
   onDelete: (item: CartItem) => void;
   onDeleteAll: () => void;
+  setSignupOpen: (isOpen: boolean) => void;
+  setLoginOpen: (isOpen: boolean) => void;
 }
 
 export default function HomeNavbar(props: HomeNavbarProps) {
-  const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props;
+  const {
+    cartItems,
+    onAdd,
+    onRemove,
+    onDelete,
+    onDeleteAll,
+    setSignupOpen,
+    setLoginOpen,
+  } = props;
   const authMember = null;
-  const [count, setCount] = useState<number>(0);
-  const [value, setValue] = useState<boolean>(true);
-
-  useEffect(() => {
-    console.log("componentDidMount useEffect"); // DATA FETCH
-    setCount(count + 1);
-    return () => {
-      console.log("ComponentWillUnmount");
-    };
-  }, [value]);
 
   /**    HANDLERS    **/
-  const buttonHandler = () => setValue(!value);
+
   return (
     <div className="home-navbar">
       <Container className="navbar-container">
@@ -76,7 +75,11 @@ export default function HomeNavbar(props: HomeNavbarProps) {
             />
             {!authMember ? (
               <Box>
-                <Button variant="contained" className="login-button">
+                <Button
+                  variant="contained"
+                  className="login-button"
+                  onClick={() => setLoginOpen(true)}
+                >
                   Login
                 </Button>
               </Box>
@@ -96,13 +99,13 @@ export default function HomeNavbar(props: HomeNavbarProps) {
               World's Most Delicious Cousine
             </Box>
             <Box className={"wel-txt"}>The Choise, not just a choice</Box>
-            <Box className={"service-txt"}>{count} hours service</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
             <Box className={"signup"}>
               {!authMember ? (
                 <Button
                   variant={"contained"}
                   className={"signup-button"}
-                  onClick={() => buttonHandler()}
+                  onClick={() => setSignupOpen(true)}
                 >
                   SIGN UP
                 </Button>
