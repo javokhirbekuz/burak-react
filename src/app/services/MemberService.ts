8c654e3faae8032a1f9216fe80ee813238ab20e4
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2025-12-14 21:45:18 +0900

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -55,11 +55,23 @@ class MemberService {
       console.log("login: ", result);
       const member: Member = result.data.member;
       console.log("member:", member);
-      localStorage.setItem("memberData:", JSON.stringify(member));
+      localStorage.setItem("memberData", JSON.stringify(member));
 
       return member;
     } catch (err) {
       console.log("Error, login:", err);
+      throw err;
+    }
+  }
+  public async logout(): Promise<void> {
+    try {
+      const url = this.path + "/member/logout";
+      const result = await axios.post(url, {}, { withCredentials: true });
+      console.log("logout: ", result);
+
+      localStorage.removeItem("memberData");
+    } catch (err) {
+      console.log("Error, logout:", err);
       throw err;
     }
   }
